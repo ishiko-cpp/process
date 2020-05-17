efe7ba3bf0da914a9cb142f6952c0a53353319fa
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-05-17 15:47:46 +0100

    Update ChildProcessBuilder.cpp

--- Source/ChildProcessBuilder.cpp
+++ Source/ChildProcessBuilder.cpp
@@ -40,7 +40,7 @@ ChildProcess ChildProcessBuilder::start(Error& error)
     else
     {
         char* argv[1];
-        int err = execv(m_commandLine.c_str(), argv);
+        int err = execv(m_commandLine.toString(), argv);
         exit(-1);
     }
 #elif defined(_WIN32)
