ef6130c94fe9d3206056af3182763a7b23e47955
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-27 22:10:09 +0000

    Fix Linux compilation

--- Source/CurrentEnvironment.cpp
+++ Source/CurrentEnvironment.cpp
@@ -6,7 +6,9 @@
 
 #include "CurrentEnvironment.h"
 #include <Ishiko/Platform/OS.h>
-#if ISHIKO_OS == ISHIKO_OS_WINDOWS
+#if ISHIKO_OS == ISHIKO_OS_LINUX
+extern char** environ;
+#elif ISHIKO_OS == ISHIKO_OS_WINDOWS
 // There seems to be an error with the definition of GetEnvironmentStringsA so we undefine UNICODE so that we can use
 // GetEnvironmentStrings
 #undef UNICODE
@@ -48,7 +50,6 @@ std::map<std::string, std::string> CurrentEnvironment::ToMap()
     std::map<std::string, std::string> result;
 
 #if ISHIKO_OS == ISHIKO_OS_LINUX
-    extern char** environ;
     for (char** p = environ; *p != nullptr; ++p)
     {
         char* eq = strchr(*p, '=');
