8524d9fb801912dfa65dae4a165feedee814a0bc
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-27 23:26:41 +0000

    Update Process.h

--- Include/Ishiko/Process.h
+++ Include/Ishiko/Process.h
@@ -7,8 +7,13 @@
 #ifndef _ISHIKO_PROCESS_H_
 #define _ISHIKO_PROCESS_H_
 
+#include "Process/ErrorCategory.h"
+#include "Process/CommandLine.h"
+#include "Process/CurrentEnvironment.h"
 #include "Process/Environment.h"
-#include "Process/ProcessHandle.h"
+#include "Process/CurrentProcess.h"
+#include "Process/ChildProcess.h"
+#include "Process/ChildProcessBuilder.h"
 
 #include "Process/linkoptions.h"
 
