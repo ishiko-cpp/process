d18fd8e00a5f7927d80f70187db9ec2dcccaa957
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-06-20 18:35:39 +0100

    Add files

---
+++ include/Ishiko/Process/CommandLineSpecification.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/process/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_PROCESS_COMMANDLINESPECIFICATION_H_
+#define _ISHIKO_PROCESS_COMMANDLINESPECIFICATION_H_
+
+#endif
