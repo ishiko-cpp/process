fd1f377f890a0b8eb5817a0ba475d0da82b1c2fc
Xavier Leclercq <xavier@leclercq.dev>
2024-05-28 21:27:28 +0100

    Fix code after changes to dependencies (#121)

--- include/Ishiko/Process/ProcessErrorCategory.hpp
+++ include/Ishiko/Process/ProcessErrorCategory.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2020-2022 Xavier Leclercq
+    Copyright (c) 2020-2024 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/process/blob/main/LICENSE.txt
 */
@@ -23,7 +23,7 @@ public:
     static const ProcessErrorCategory& Get() noexcept;
 
     const char* name() const noexcept override;
-    std::ostream& streamOut(int value, std::ostream& os) const override;
+    const char* message(int ev, char* buffer, size_t len) const noexcept override;
 
 private:
     ProcessErrorCategory() noexcept = default;
